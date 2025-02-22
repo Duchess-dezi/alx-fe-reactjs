7385197edc494ef3d60a0f9143c6a6ad32bdd0b8
Christabel <dezinyerhovwo@gmail.com>
2025-02-22 21:49:41 +0100

    created a new directory alx-react-app-new

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+    return (
+        <footer style={{ textAlign: 'center', fontWeight: 'bold', backgroundColor: '#000' }}>
+            <p>© 2023 City Lovers</p>
+        </footer>
+    );
+}
+
+export default Footer;
---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,10 @@
+
+function MainContent() {
+    return (
+        <main>
+            <p style={{ backgroundColor: '#333', fontWeight: 'bold', fontSize: '36px', color: 'white' }}>I love to visit New York, Paris, and Tokyo.</p>
+        </main>
+    );
+}
+
+export default MainContent;
