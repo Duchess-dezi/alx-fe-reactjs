b547ebaa00399887bf048c572d599f38794c5701
Christabel <dezinyerhovwo@gmail.com>
2025-02-22 22:55:12 +0100

    Updated App.jsx

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -3,13 +3,12 @@ import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
 import UserProfile from './components/UserProfile';
-import { useState } from 'react'
+import Counter from './components/Counter';
 import './App.css'
 
 
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
@@ -18,14 +17,9 @@ function App() {
         <Header />
         <MainContent />
         <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
-
-        <div className="card">
-          <button onClick={() => setCount((count) => count + 1)}>
-            count is {count}
-          </button>
-        </div>
+        <Counter />
+        <Footer />
       </div>
-      <Footer />
     </>
   );
 }
