850c53b8cd9208c0f01352a2ecf875c963cd77df
Christabel <dezinyerhovwo@gmail.com>
2025-02-23 22:13:22 +0100

    commited new changes

--- alx-react-app-props/src/UserDetails.jsx
+++ alx-react-app-props/src/UserDetails.jsx
@@ -1,10 +1,10 @@
 import React, { useContext } from "react";
-import userContext from "../UserContext";
+import UserContext from "./UserContext";
 
 
 function UserDetails({ userData }) {
 
-    const userData = useContext(userContext);
+    const userData = useContext(UserContext);
     return (
         <div>
             <p>Name: {userData.name}</p>
