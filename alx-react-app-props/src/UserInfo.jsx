c29b205094c2961e6ec49e60dbcd298ea4c65873
Christabel <dezinyerhovwo@gmail.com>
2025-02-23 15:46:09 +0100

    duplicated alx-react-app as alx-react-app-props

---
+++ alx-react-app-props/src/UserInfo.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import UserDetails from './UserDetails';
+
+function UserInfo({ userData }) {
+    return
+    <>
+        <UserDetails userData={userData} />;
+    </>
+}
+
+export default UserInfo;
