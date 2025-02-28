e9dd08a8897468a8e01df81f4a270daaa92a4886
Christabel <dezinyerhovwo@gmail.com>
2025-02-28 23:08:39 +0100

    modified RecipeList

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,11 +1,9 @@
 import { useState } from 'react'
 import './App.css'
-import RecipeList from './components/recipeList'
+import RecipeList from './components/RecipeList'
 import AddRecipeForm from './components/AddRecipeForm'
 
 function App() {
-
-
   return (
     <>
       <RecipeList />
