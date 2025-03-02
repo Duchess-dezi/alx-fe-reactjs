ec210a7d5fa667c9c4ee4fba8d1ed961504e6787
Christabel <dezinyerhovwo@gmail.com>
2025-03-02 20:14:10 +0100

    Changed FavouritesList to FavoritesList.jsx

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -2,7 +2,7 @@ import React, { useMemo } from 'react';
 import { Link } from 'react-router-dom'; // ✅ Import Link
 import useRecipeStore from './recipeStore';
 import SearchBar from './SearchBar.jsx';
-import FavoritesList from './FavouritesList';
+import FavoritesList from './FavoritesList';
 import RecommendationsList from './RecommendationsList';
 
 const RecipeList = () => {
