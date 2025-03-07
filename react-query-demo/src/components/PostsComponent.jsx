de4fabe631d9440c88f3586302ff0637056ceee6
Christabel <dezinyerhovwo@gmail.com>
2025-03-07 17:16:45 +0100

    Fixed fetchPost and isError syntax

--- react-query-demo/src/components/PostsComponent.jsx
+++ react-query-demo/src/components/PostsComponent.jsx
@@ -1,7 +1,7 @@
 import { useQuery } from "react-query";
 import React from 'react'
-//define a fetched function that can be used to fetch data from an API
-const fetchData = async () => {
+//define a fetched function that can be used to fetch posts from an API
+const fetchPosts = async () => {
     const res = await fetch('https://jsonplaceholder.typicode.com/posts')
     if (!res.ok) {
         throw new Error("Failed to fetch data");
@@ -12,9 +12,9 @@ const fetchData = async () => {
 const PostsComponent = () => {
 
     //use query hook to handle data fetching and caching
-    const { data, error, isLoading, refetch } = useQuery({
-        queryKey: ["fetchData"], // Fixed useQuery key
-        queryFn: fetchData,
+    const { data, isError, isLoading, refetch } = useQuery({
+        queryKey: ["fetchPosts"], // Fixed useQuery key
+        queryFn: fetchPosts,
         staleTime: 60000,  // Cache remains fresh for 1 minute (60000ms)
         refetchOnWindowFocus: true,  // Auto refetch when window is focused
     });
@@ -23,7 +23,7 @@ const PostsComponent = () => {
     if (isLoading) return <div>Loading...</div>
 
     //handle error state
-    if (error) return <div>Error Loading Data</div>
+    if (isError) return <div>Error Loading Data</div>
 
     //to render the fetched data
     return (
